93017ae4df333799ad55775843cafb633fb12b9a
AtiqueAsif <atiqueasif10@gmail.com>
2025-04-03 12:06:19 +0600

    Stack (push, pop, display) (array).c

--- Stack pop (array).c
+++ Stack pop (array).c
@@ -1,71 +1,53 @@
-// mainly pop and display code but push is also included
+#include <stdio.h>
 
-#include<stdio.h>
-int maxSize = 8;
-int stack [8];
+int size;
 int top = -1;
+int stack[100];
 
-
-int isempty(){
-    if(top == -1){
-        return 1;
+void push(int data) {
+    if (top == size - 1) {
+        printf("Stack is full!\n");
+        return;
     }
-    else{
-        return 0;
-    }
+    top = top + 1;
+    stack[top] = data;
 }
 
-int pop(){
-    int data;
-    if(! isempty()){
-        data = stack [top];
-        top = top - 1;
-        return data;
-    }
-    else{
+void pop() {
+    if (top == -1) {
         printf("Stack is empty!\n");
-        return -1;
+        return;
     }
+    printf("Popped element: %d\n", stack[top]);
+    top = top - 1;
 }
 
-int isfull(){
-    if (top == maxSize - 1){
-        return 1;
+void display() {
+    if (top == -1) {
+        printf("Stack is empty!\n");
+        return;
     }
-    else{
-        return 0;
-    }
-}
-
-int push(int data){
-    if(! isfull()){
-        top = top + 1;
-        stack[top] = data;
-    }
-    else{
-        printf("Stack is full!!!\n");
-    }
-}
-
-int main(){
-    push(1);
-    push(2);
-    push(3);
-    push(4);
-
-    printf("Pushed elements: ");
-    for(int i = 0; i <= top; i++){
+    for (int i = 0; i <= top; i++) {
         printf("%d ", stack[i]);
     }
     printf("\n");
+}
 
+int main() {
+    printf("Enter the size of the stack: ");
+    scanf("%d", &size);
 
-    printf("Popped elements: ");
-    while (!isempty()) {
-        printf("%d ", pop());
-    }
+    push(10);
+    push(20);
+    push(30);
 
+    printf("\nAfter pushing elements:\n");
+    display();
+
+    pop();
+
+    printf("\nAfter popping an element:\n");
+    display();
 
     return 0;
-
 }
